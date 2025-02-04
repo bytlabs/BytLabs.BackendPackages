4efe5418bf3f1e937cce199703f9c5136c438220
kudchikarsk <kudchikarsk@gmail.com>
2025-02-04 12:40:06 +0530

    Add Generic DomainEventBase

--- src/BytLabs.Core/BytLabs.Domain/DomainEvents/DomainEventBase.cs
+++ src/BytLabs.Core/BytLabs.Domain/DomainEvents/DomainEventBase.cs
@@ -5,3 +5,15 @@ public abstract class DomainEventBase : IDomainEvent
     public DateTime? CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
 }
+
+public abstract class DomainEventBase<T> : DomainEventBase
+{
+    public DomainEventBase(Guid id, T data)
+    {
+        Id = id;
+        Data = data;
+    }
+
+    public Guid Id { get; private set; }
+    public T Data { get; private set; }
+}
