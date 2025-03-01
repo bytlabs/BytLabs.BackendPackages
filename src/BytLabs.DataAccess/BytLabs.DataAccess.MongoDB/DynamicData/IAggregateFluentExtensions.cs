fbbecda132b1114d67c24ff41768dcaf483a027c
kudchikarsk <kudchikarsk@gmail.com>
2025-03-01 23:03:30 +0530

    Update aggregatefluent match logic

--- src/BytLabs.DataAccess/BytLabs.DataAccess.MongoDB/DynamicData/IAggregateFluentExtensions.cs
+++ src/BytLabs.DataAccess/BytLabs.DataAccess.MongoDB/DynamicData/IAggregateFluentExtensions.cs
@@ -10,10 +10,10 @@ public static class IAggregateFluentExtensions
     public static IAggregateFluent<T> ExcludeSoftDeletedEntites<T>(this IAggregateFluent<T> aggregateFluent)
         where T : ISoftDeletable
     {
-        return aggregateFluent.Match(Builders<T>.Filter.Where(entity => !entity.IsDeleted));
+        return aggregateFluent.Match(Builders<T>.Filter.Ne(entity => entity.IsDeleted, true));
     }
 
-    public static IAggregateFluent<T> ApplyDynamicDataFilteration<T>(this IAggregateFluent<T> aggregateFluent, InputFilteringDynamicData? dataFilter)
+    public static IAggregateFluent<T> ApplyDynamicDataFilteration<T>(this IAggregateFluent<T> aggregateFluent, InputFilteringDynamicData dataFilter)
         where T : IHaveDynamicData
     {
         var matchDataFilter = dataFilter is null ? Builders<T>.Filter.Empty
