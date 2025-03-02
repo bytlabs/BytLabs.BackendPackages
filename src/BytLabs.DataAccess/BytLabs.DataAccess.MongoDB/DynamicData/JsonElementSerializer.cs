6576078ad931b79cf6338ca3d9c61f17b82682f4
Shadman Kudchikar <kudchikarsk@gmail.com>
2025-03-02 15:33:58 +0530

    Exclude dynamic data filteration (#20)

--- src/BytLabs.DataAccess/BytLabs.DataAccess.MongoDB/DynamicData/JsonElementSerializer.cs
+++ src/BytLabs.DataAccess/BytLabs.DataAccess.MongoDB/DynamicData/JsonElementSerializer.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Bson;
 using System.Globalization;
+using MongoDB.Bson.IO;
 
 namespace BytLabs.DataAccess.MongoDB.DynamicData
 {
@@ -10,6 +11,12 @@ namespace BytLabs.DataAccess.MongoDB.DynamicData
     {
         public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, JsonElement value)
         {
+            if (value.ValueKind == JsonValueKind.Null || value.ValueKind == JsonValueKind.Undefined)
+            {
+                context.Writer.WriteNull();
+                return;
+            }
+
             var jsonString = value.ToString();
             var bsonDocument = BsonDocument.Parse(jsonString);
             bsonDocument.ConvertIsoStringsToBsonDates();
@@ -18,6 +25,12 @@ namespace BytLabs.DataAccess.MongoDB.DynamicData
 
         public override JsonElement Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
         {
+            if (context.Reader.CurrentBsonType == BsonType.Null || context.Reader.CurrentBsonType == BsonType.Undefined)
+            {
+                context.Reader.ReadNull();
+                return default;
+            }
+
             var data = BsonSerializer.Deserialize<BsonDocument>(context.Reader);
             data.ConvertBsonDatesToIsoStrings();
             var jsonString = data.ToString();
